98145512213dc424f4550d3d53634c1dc494f6f0
KomalYadav1711 <komalyadav11117@gmail.com>
2026-07-03 20:21:44 +0530

    feat: setup design system foundation

---
+++ src/app/constants/site.ts
@@ -0,0 +1,13 @@
+export const siteConfig = {
+
+    name: "CBS Software",
+    description: "Transforming Businesses Through Smart Digital Solutions",
+    url: "https://cbs-software.vercel.app",
+    links: {
+         facebook: "",
+         linkedin: "",
+         instagram: "",
+
+
+    }
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,19 @@
 import Image from "next/image";
+import { Container } from "./components/common/container";
 
 export default function Home() {
   return (
     <main>
-      <h1>CBS Software</h1>
+      <Container className="py-32">
+        <h1 className="text-6xl font-bold">
+          CBS Software
+        </h1>
+
+        <p className="mt-6 text-xl text-slate-600">
+          Transforming Businesses Through Smart Digital Solutions
+        </p>
+
+      </Container>
     </main>
   );
 }

1b2633cefcfb1017c8bd05ef9d4d4361396081e8
KomalYadav1711 <komalyadav11117@gmail.com>
2026-07-15 20:39:25 +0530

    feat: add Google Analytics scripts for tracking and analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import "./globals.css";
 import { Navbar } from "../components/layout/navbar/navbar";
 import { Footer } from "@/components/layout/footer";
+import Script from "next/script";
 
 export const metadata: Metadata = {
   title: "CBS Software",
@@ -20,6 +21,19 @@ export default function RootLayout({
         {children}
         <Footer/>
       </body>
+      <Script
+        src="https://www.googletagmanager.com/gtag/js?id=G-LCVVVRLPVT"
+        strategy="afterInteractive"
+      />
+      <Script id="google-analytics" strategy="afterInteractive">
+        {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+
+          gtag('config', 'G-LCVVVRLPVT');
+        `}
+      </Script>
     </html>
   );
 }
